282550c40248bf8f226d1827742b958d878731fc
Vitaliy <mitradis@gmail.com>
2022-05-23 18:17:32 +0300

    ReUpload

---
+++ FilesList/FormMain.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace FilesList
+{
+    partial class FormMain
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Путь";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(176, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(132, 20);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Атрибуты файла";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox2.Location = new System.Drawing.Point(176, 30);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(132, 20);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "CRC32 сумма";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 61);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Files List";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
+
---
+++ FilesList/FormMain.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FilesList
+{
+    public partial class FormMain : Form
+    {
+        List<string> filesList = new List<string>();
+        string logOut = pathAddSlash(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)) + "_list.txt";
+        string lastFolder = null;
+        int pathLength = 0;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (lastFolder != null)
+            {
+                if (Directory.Exists(lastFolder))
+                {
+                    folderBrowserDialog1.SelectedPath = lastFolder;
+                }
+            }
+            else
+            {
+                folderBrowserDialog1.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                if (Directory.Exists(folderBrowserDialog1.SelectedPath))
+                {
+                    lastFolder = folderBrowserDialog1.SelectedPath;
+                    if (folderBrowserDialog1.SelectedPath.EndsWith(@"\") || folderBrowserDialog1.SelectedPath.EndsWith("/"))
+                    {
+                        pathLength = folderBrowserDialog1.SelectedPath.Length;
+                    }
+                    else
+                    {
+                        pathLength = folderBrowserDialog1.SelectedPath.Length + 1;
+                    }
+                    searchFolder(folderBrowserDialog1.SelectedPath);
+                    try
+                    {
+                        File.WriteAllLines(logOut, filesList);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Не удалось записать файл: " + logOut + Environment.NewLine + ex.Message);
+                    }
+                }
+            }
+            filesList.Clear();
+        }
+
+        private void searchFolder(string path)
+        {
+            getFilesList(path);
+            foreach (string line in Directory.GetDirectories(path))
+            {
+                if (!new DirectoryInfo(line).Attributes.HasFlag(FileAttributes.System))
+                {
+                    searchFolder(line);
+                }
+            }
+        }
+
+        private void getFilesList(string path)
+        {
+            foreach (string line in Directory.GetFiles(path))
+            {
+                if (checkBox1.Checked)
+                {
+                    if (checkBox2.Checked)
+                    {
+                        filesList.Add(line.Remove(0, pathLength) + "\t" + getCRC(line) + "\t" + new FileInfo(line).Length + "\t" + new FileInfo(line).LastWriteTime);
+                    }
+                    else
+                    {
+                        filesList.Add(line.Remove(0, pathLength) + "\t" + new FileInfo(line).Length + "\t" + new FileInfo(line).LastWriteTime);
+                    }
+                }
+                else
+                {
+                    if (checkBox2.Checked)
+                    {
+                        filesList.Add(line.Remove(0, pathLength) + "\t" + getCRC(line));
+                    }
+                    else
+                    {
+                        filesList.Add(line.Remove(0, pathLength));
+                    }
+                }
+            }
+        }
+
+        private string getCRC(string file)
+        {
+            FileStream streamFile = File.OpenRead(file);
+            string line = string.Format("{0:X}", calculateCRC(streamFile));
+            streamFile.Close();
+            while (line.Length != 8)
+            {
+                line = "0" + line;
+            }
+            return line;
+        }
+
+        private static uint calculateCRC(Stream stream)
+        {
+            const int buffer_size = 1024;
+            const uint POLYNOMIAL = 0xEDB88320;
+            uint result = 0xFFFFFFFF;
+            uint Crc32;
+            byte[] buffer = new byte[buffer_size];
+            uint[] table_CRC32 = new uint[256];
+            unchecked
+            {
+                for (int i = 0; i < 256; i++)
+                {
+                    Crc32 = (uint)i;
+                    for (int j = 8; j > 0; j--)
+                    {
+                        if ((Crc32 & 1) == 1)
+                        {
+                            Crc32 = (Crc32 >> 1) ^ POLYNOMIAL;
+                        }
+                        else
+                        {
+                            Crc32 >>= 1;
+                        }
+                    }
+                    table_CRC32[i] = Crc32;
+                }
+                int count = stream.Read(buffer, 0, buffer_size);
+                while (count > 0)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        result = ((result) >> 8) ^ table_CRC32[(buffer[i]) ^ ((result) & 0x000000FF)];
+                    }
+                    count = stream.Read(buffer, 0, buffer_size);
+                }
+            }
+            stream.Close();
+            return ~result;
+        }
+
+        private static string pathAddSlash(string path)
+        {
+            if (!path.EndsWith("/") && !path.EndsWith(@"\"))
+            {
+                if (path.Contains("/"))
+                {
+                    path += "/";
+                }
+                else if (path.Contains(@"\"))
+                {
+                    path += @"\";
+                }
+            }
+            return path;
+        }
+    }
+}
